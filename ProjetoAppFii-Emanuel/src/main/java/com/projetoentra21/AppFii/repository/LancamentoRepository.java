e29a10aa10fb1e2080c56e7316f827706bb57a8d
Ana <92342881+anadognini@users.noreply.github.com>
2022-09-23 10:40:34 -0300

    Add files via upload

---
+++ ProjetoAppFii-Emanuel/src/main/java/com/projetoentra21/AppFii/repository/LancamentoRepository.java
@@ -0,0 +1,11 @@
+package com.projetoentra21.AppFii.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoentra21.AppFii.models.Lancamento;
+
+public interface LancamentoRepository extends JpaRepository<Lancamento,Long>{
+	
+	
+
+}
